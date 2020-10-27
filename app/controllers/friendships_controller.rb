628eafcb95d0695a5b5a5dd03c3e4fc3a138a9d7
Gavin Deas <gavin.deas@comestri.com>
2020-10-27 12:08:30 +0000

    Add "follow friend" functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
+    # friend_id = params[:friend]
+    # byebug
+    current_user.friendships.build(friend_id: params[:friend])
+    if current_user.save
+      flash[:notice] = 'Following user'
+    else
+      flash[:alert] = 'Something went wrong with the request'
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
